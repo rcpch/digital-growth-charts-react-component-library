323284c83e03ffa4db3a3c5abfdc79cb8c6100c2
Simon Chapman <eatyourpeasapps@gmail.com>
2021-08-02 22:39:21 +0100

    sort measurements in isCrowded

--- src/functions/isCrowded.ts
+++ src/functions/isCrowded.ts
@@ -6,6 +6,7 @@ export function isCrowded(domains: any, childMeasurements: Measurement[]) {
     if (childMeasurements.length < 2) {
         return false;
     }
+    childMeasurements.sort((a,b)=> (a.measurement_dates.corrected_decimal_age > b.measurement_dates.corrected_decimal_age) ? 1 : (a.measurement_dates.corrected_decimal_age < b.measurement_dates.corrected_decimal_age) ? -1 : 0 )
     let smallestInterval: null | number = null;
     for (let i = 0; i < childMeasurements.length; i++) {
         const lowerValue: number = childMeasurements[i].plottable_data.centile_data.chronological_decimal_age_data.x;
