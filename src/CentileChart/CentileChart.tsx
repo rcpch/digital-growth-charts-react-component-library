ced5017a5723add695e7cede1674247915c8a8bb
eatyourpeas <simon@eatyourpeas.co.uk>
2024-03-17 22:24:33 +0000

    tidy up

--- src/CentileChart/CentileChart.tsx
+++ src/CentileChart/CentileChart.tsx
@@ -282,7 +282,7 @@ function CentileChart({
                                     pointerLength={5}
                                     cornerRadius={0}
                                     flyoutStyle={styles.toolTipFlyout}
-                                    style={{textAnchor:'start'}}
+                                    style={{textAnchor:'end'}}
                                 />
                             }
                             labels={({ datum }) => {
