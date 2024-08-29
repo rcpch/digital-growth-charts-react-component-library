ed11b37f8b0a0fe119b049f7720c47c1a3fedc70
eatyourpeas <simon@eatyourpeas.co.uk>
2024-08-29 15:25:26 +0100

    update fonts

--- fonts/fonts.ts
+++ fonts/fonts.ts
@@ -1,5 +1,5 @@
 import Montserrat from './Montserrat/Montserrat-Regular.ttf';
-import Arial from './Arial/ARI.ttf'
+import Arial from './Arial/ArialRegularArialRegular.ttf';
 const montserratRegular = Montserrat
 const arialRegular = Arial
 const fonts = {
