6f32ef4fe6e59fda4a15550352e1d7d094d172f1
eatyourpeas <simon@eatyourpeas.co.uk>
2024-10-18 20:31:26 +0100

    fix label text for nondisjunction lines

--- src/CentileChart/CentileChart.tsx
+++ src/CentileChart/CentileChart.tsx
@@ -824,7 +824,7 @@ function CentileChart({
                                                     angle={-90}
                                                     dx={5}
                                                     dy={10}
-                                                    style={styles.nondisjunctionThresholdLabel}
+                                                    style={styles.delayedPubertyThresholdLabel}
                                                 />
                                             }
                                         />
