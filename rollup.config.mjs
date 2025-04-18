a901fb54ef3fdd184e5eb7254da0027f0af74192
eatyourpeas <simon@eatyourpeas.co.uk>
2025-04-18 22:14:22 +0100

    functioning umd build with demo.html

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,12 @@
+import alias from '@rollup/plugin-alias';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import dts from 'rollup-plugin-dts';
 import image from '@rollup/plugin-image';
 import json from '@rollup/plugin-json';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import replace from '@rollup/plugin-replace';
+import path from 'path';
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
@@ -35,26 +38,11 @@ export default [
                 file: packageJson.main,
                 format: 'cjs',
                 sourcemap: true,
-                exports: 'named',
             },
             {
                 file: packageJson.module,
                 format: 'esm',
                 sourcemap: true,
-                exports: 'named',
-            },
-            {
-                file: 'build/umd/rcpch-digital-growth-charts.umd.js',
-                format: 'umd',
-                name: 'RCPCHGrowthCharts',
-                sourcemap: true,
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM',
-                    'react-dom/client': 'ReactDOM',
-                    'styled-components': 'styled',
-                },
-                exports: 'named', // Ensure named exports
             },
         ],
         plugins: [
@@ -68,17 +56,15 @@ export default [
                 ignoreGlobal: true,
                 include: /\/node_modules\//,
             }),
-            babel({
-                exclude: 'node_modules/**',
-                babelHelpers: 'bundled',
-                presets: ['@babel/preset-react', '@babel/preset-typescript'],
-                extensions: ['.ts', '.tsx'],
+            typescript(),
+            terser({
+                compress: {
+                    pure_getters: true, // assume obj.prop has no side effects
+                    dead_code: true,
+                    toplevel: true,
+                },
+                mangle: true,
             }),
-            typescript({
-                // Remove jsx: 'react' here
-                target: 'es5',
-            }),
-            terser(),
             json(),
             versionInjector(),
             image(),
@@ -86,8 +72,62 @@ export default [
     },
     {
         input: 'src/index.ts',
-        output: [{ file: 'build/types.d.ts', format: 'es' }],
         external: [],
+        output: [{ file: 'build/types.d.ts', format: 'es' }],
         plugins: [dts.default()],
+    },
+    {
+        input: 'src/umd.ts',
+        external: ['react', 'react-dom', 'react-dom/client'],
+        output: [
+            {
+                file: 'build/umd/rcpch-digital-growth-charts.umd.js',
+                format: 'umd',
+                name: 'RCPCHGrowthCharts',
+                exports: 'default',
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                    'react-dom/client': 'ReactDOM',
+                },
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            alias({
+                entries: [
+                    {
+                        find: 'process',
+                        replacement: path.resolve(__dirname, 'src/stubs/process.js'),
+                    },
+                ],
+            }),
+            replace({
+                preventAssignment: true,
+                values: {
+                    'process.env.NODE_ENV': JSON.stringify('production'),
+                },
+            }),
+            postcss({
+                extensions: ['.css'],
+                inject: true,
+            }),
+            resolve(),
+            commonjs({
+                ignoreGlobal: true,
+                include: /\/node_modules\//,
+            }),
+            babel({
+                exclude: 'node_modules/**',
+                babelHelpers: 'bundled',
+                presets: ['@babel/preset-react', '@babel/preset-typescript'],
+                extensions: ['.ts', '.tsx'],
+            }),
+            typescript(),
+            terser(),
+            json(),
+            versionInjector(),
+            image(),
+        ],
     },
 ];
---
+++ src/umd.ts
@@ -0,0 +1,33 @@
+import React, { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RCPCHChartProps } from './RCPCHChart/RCPCHChart.types';
+import RCPCHChart from './RCPCHChart';
+// This function is used to render the RCPCHChart component into a target element
+// in the DOM. It takes an object with the target element ID and chart properties.
+// It is designed to be used in a non-React environment, such as a static HTML page
+// or a server-rendered page where React is not the main framework.
+// The function first checks if the target element exists in the DOM. If it does,
+// it creates a React root and renders the RCPCHChart component into that element.
+// If the target element is not found, it logs an error message to the console.
+interface RenderChartOptions extends Omit<RCPCHChartProps, 'children'> {
+    targetElementId: string;
+}
+
+const renderRCPCHChart = (options: RenderChartOptions): void => {
+    const { targetElementId, ...chartProps } = options;
+    const targetElement = document.getElementById(targetElementId);
+
+    if (!targetElement) {
+        console.error(`Target element with ID '${targetElementId}' not found.`);
+        return;
+    }
+
+    const root = createRoot(targetElement);
+    root.render(createElement(RCPCHChart, { ...chartProps }));
+};
+
+const RCPCHGrowthCharts = {
+    render: renderRCPCHChart,
+};
+
+export default RCPCHGrowthCharts;
